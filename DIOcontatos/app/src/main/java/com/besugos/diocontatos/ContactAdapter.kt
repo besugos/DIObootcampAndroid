382a7cd654ecc3ab651438e5b513c0a7188cdb1c
Wendel Bandeira <besugos@gmail.com>
2021-03-10 18:16:01 -0300

    DIO contacts

--- DIOcontatos/app/src/main/java/com/besugos/diocontatos/ContactAdapter.kt
+++ DIOcontatos/app/src/main/java/com/besugos/diocontatos/ContactAdapter.kt
@@ -1,4 +1,35 @@
 package com.besugos.diocontatos
 
-class ContactAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter (val contactsList: ArrayList<Contact>): RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+        holder.bindItem(contactsList[position])
+    }
+
+    override fun getItemCount(): Int = contactsList.size
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(contact: Contact) {
+
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_phone)
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+        }
+
+    }
+
+
 }
